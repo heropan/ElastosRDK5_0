767303004dd0f457b97190438c8e7d52fa12b7dc
Shen Jianliang <shen.jianliang@kortide.com>
2015-06-25 15:46:25 +0800

    Add Interface ITransliterator to compiling list.

--- Sources/Elastos/LibCore/src/libcore/icu/icu.cpp
+++ Sources/Elastos/LibCore/src/libcore/icu/icu.cpp
@@ -2,6 +2,7 @@
 #include "CCollationKeyICU.h"
 #include "CLocaleData.h"
 #include "CAlphabeticIndex.h"
+#include "CTransliterator.h"
 
 namespace Libcore {
 namespace ICU {
@@ -11,6 +12,8 @@ CAR_OBJECT_IMPL(CCollationKeyICU)
 CAR_OBJECT_IMPL(CLocaleData)
 
 CAR_OBJECT_IMPL(CAlphabeticIndex)
+
+CAR_OBJECT_IMPL(CTransliterator)
 
 } // namespace ICU
 } // namespace Libcore
