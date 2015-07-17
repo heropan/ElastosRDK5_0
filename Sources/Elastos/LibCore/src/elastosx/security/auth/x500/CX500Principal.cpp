480cc4b8f28fa98a7904f7e6fea4803e8473c3b9
Shen Jianliang <shen.jianliang@kortide.com>
2015-07-17 15:58:07 +0800

    Add IX500Principal

--- Sources/Elastos/LibCore/src/elastosx/security/auth/x500/CX500Principal.cpp
+++ Sources/Elastos/LibCore/src/elastosx/security/auth/x500/CX500Principal.cpp
@@ -1,7 +1,8 @@
 
 #include "CX500Principal.h"
 #include "CStringWrapper.h"
-#include <StringBuilder.h>
+#include "StringBuilder.h"
+#include "AutoLock.h"
 
 using Elastos::Core::CStringWrapper;
 using Elastos::Core::ICharSequence;
@@ -12,31 +13,50 @@ namespace Security {
 namespace Auth {
 namespace X500 {
 
+CAR_OBJECT_IMPL(CX500Principal)
+
+#if 0 // TODO: Waiting for IPrincipal
+CAR_INTERFACE_IMPL_2(CX500Principal, Object, IX500Principal, IPrincipal)
+#else
+CAR_INTERFACE_IMPL(CX500Principal, Object, IX500Principal)
+#endif
+
+const Int64 mSerialVersionUID = -500463348111345721L;
+
 ECode CX500Principal::GetEncoded(
-    /* [out, callee] */ ArrayOf<Byte> **encodedName)
+    /* [out, callee] */ ArrayOf<Byte>** encodedName)
 {
-    //Apache...Todo later
-    /*
-        byte[] src = dn.getEncoded();
-        byte[] dst = new byte[src.length];
-        System.arraycopy(src, 0, dst, 0, dst.length);
-        return dst;
-    */
-    return E_NOT_IMPLEMENTED;
+    VALIDATE_NOT_NULL(*encodedName)
+    *encodedName = NULL;
+
+    AutoPtr<ArrayOf<Byte> > src;
+#if 0 // TODO: Waiting for IName
+    FAIL_RETURN(mDn->GetEncoded((ArrayOf<Byte>**)&src));
+#else
+    assert(0);
+#endif
+    AutoPtr<ArrayOf<Byte> > dst = ArrayOf<Byte>::Alloc(src->GetLength());
+    dst->Copy(0, src, 0, dst->GetLength());
+    *encodedName = dst;
+    return NOERROR;
 }
 
 ECode CX500Principal::GetName(
     /* [in] */ const String& format,
     /* [out] */ String *name)
 {
+    VALIDATE_NOT_NULL(name)
+    *name = String(NULL);
+
     if (CANONICAL.Equals(format)) {
         return GetCanonicalName(name);
     }
-    //Apache...Todo later
-    /*
-        return dn.getName(format);
-    */
-    return E_NOT_IMPLEMENTED;
+#if 0 // TODO: Waiting for Iname
+    return mDn->GetName(format, name);
+#else
+    assert(0);
+    return NOERROR;
+#endif
 }
 
 ECode CX500Principal::GetName(
@@ -44,69 +64,94 @@ ECode CX500Principal::GetName(
     /* [in] */ IMap *oidMap,
     /* [out] */ String * pName)
 {
-    //Apache...Todo later
-    /*
-        String rfc1779Name = dn.getName(RFC1779);
-        String rfc2253Name = dn.getName(RFC2253);
+    VALIDATE_NOT_NULL(pName);
+    *pName = String(NULL);
+    VALIDATE_NOT_NULL(oidMap)
 
-        if (format.equalsIgnoreCase("RFC1779")) {
-            StringBuilder resultName = new StringBuilder(rfc1779Name);
-            int fromIndex = resultName.length();
-            int equalIndex = -1;
-            while (-1 != (equalIndex = resultName.lastIndexOf("=", fromIndex))) {
-                int commaIndex = resultName.lastIndexOf(",", equalIndex);
-                String subName = resultName.substring(commaIndex + 1,
-                        equalIndex).trim();
-                if (subName.length() > 4
-                        && subName.substring(0, 4).equals("OID.")) {
-                    String subSubName = subName.substring(4);
-                    if (oidMap.containsKey(subSubName)) {
-                        String replaceName = oidMap.get(subSubName);
-                        if(commaIndex > 0) replaceName = " " + replaceName;
-                        resultName.replace(commaIndex + 1, equalIndex, replaceName);
-                    }
-                }
-                fromIndex = commaIndex;
-            }
-            return resultName.toString();
-        } else if (format.equalsIgnoreCase("RFC2253")) {
-            StringBuilder resultName = new StringBuilder(rfc2253Name);
-            StringBuilder subsidyName = new StringBuilder(rfc1779Name);
+    String rfc1779Name;
+#if 0 // TODO: Waiting for Iname
+    mDn->GetName(RFC1779, &rfc1779Name);
+#else
+    assert(0);
+#endif
+    String rfc2253Name;
+#if 0 // TODO: Waiting for Iname
+    mDn->GetName(RFC2253, &rfc2253Name);
+#else
+    assert(0);
+#endif
 
-            int fromIndex = resultName.length();
-            int subsidyFromIndex = subsidyName.length();
-            int equalIndex = -1;
-            int subsidyEqualIndex = -1;
-            while (-1 != (equalIndex = resultName.lastIndexOf("=", fromIndex))) {
-                subsidyEqualIndex = subsidyName.lastIndexOf("=",
-                        subsidyFromIndex);
-                int commaIndex = resultName.lastIndexOf(",", equalIndex);
-                String subName = resultName.substring(commaIndex + 1,
-                        equalIndex).trim();
-                if (oidMap.containsKey(subName)) {
-                    int subOrignalEndIndex = resultName
-                            .indexOf(",", equalIndex);
-                    if (subOrignalEndIndex == -1)
-                        subOrignalEndIndex = resultName.length();
-                    int subGoalEndIndex = subsidyName.indexOf(",",
-                            subsidyEqualIndex);
-                    if (subGoalEndIndex == -1)
-                        subGoalEndIndex = subsidyName.length();
-                    resultName.replace(equalIndex + 1, subOrignalEndIndex,
-                            subsidyName.substring(subsidyEqualIndex + 1,
-                                    subGoalEndIndex));
-                    resultName.replace(commaIndex + 1, equalIndex, oidMap
-                            .get(subName));
+    if (format.EqualsIgnoreCase("RFC1779")) {
+        AutoPtr<StringBuilder> resultName = new StringBuilder(rfc1779Name);
+        Int32 fromIndex = resultName->GetLength();
+        Int32 equalIndex = -1;
+        while (-1 != (resultName->LastIndexOf(String("="), fromIndex, &equalIndex), equalIndex)) {
+            Int32 commaIndex;
+            resultName->LastIndexOf(String(","), equalIndex, &commaIndex);
+            String subName = resultName->Substring(commaIndex + 1,
+                    equalIndex).Trim();
+            if (subName.GetLength() > 4
+                    && subName.Substring(0, 4).Equals("OID.")) {
+                String subSubName = subName.Substring(4);
+                AutoPtr<ICharSequence> csq;
+                CStringWrapper::New(subSubName, (ICharSequence**)&csq);
+                Boolean b;
+                if (oidMap->ContainsKey(csq, &b), b) {
+                    String replaceName;
+                    AutoPtr<ICharSequence> pReplaceName;
+                    oidMap->Get(csq, (IInterface**)(ICharSequence**)&pReplaceName);
+                    pReplaceName->ToString(&replaceName);
+                    if(commaIndex > 0) replaceName = String(" ") + replaceName;
+                    resultName->Replace(commaIndex + 1, equalIndex, replaceName);
                 }
-                fromIndex = commaIndex;
-                subsidyFromIndex = subsidyEqualIndex - 1;
             }
-            return resultName.toString();
-        } else {
-            throw new IllegalArgumentException("invalid format specified: " + format);
+            fromIndex = commaIndex;
         }
-    */
-    return E_NOT_IMPLEMENTED;
+        resultName->ToString(pName);
+        return NOERROR;
+    } else if (format.EqualsIgnoreCase("RFC2253")) {
+        AutoPtr<StringBuilder> resultName = new StringBuilder(rfc2253Name);
+        AutoPtr<StringBuilder> subsidyName = new StringBuilder(rfc1779Name);
+
+        Int32 fromIndex = resultName->GetLength();
+        Int32 subsidyFromIndex = subsidyName->GetLength();
+        Int32 equalIndex = -1;
+        Int32 subsidyEqualIndex = -1;
+        while (-1 != (resultName->LastIndexOf(String("="), fromIndex, &equalIndex), equalIndex)) {
+            subsidyName->LastIndexOf(String("="), subsidyFromIndex, &subsidyEqualIndex);
+            Int32 commaIndex;
+            resultName->LastIndexOf(String(","), equalIndex, &commaIndex);
+            String subName = resultName->Substring(commaIndex + 1, equalIndex).Trim();
+            AutoPtr<ICharSequence> csq;
+            CStringWrapper::New(subName, (ICharSequence**)&csq);
+            Boolean b;
+            if (oidMap->ContainsKey(csq, &b), b) {
+                Int32 subOrignalEndIndex;
+                resultName->IndexOf(String(","), equalIndex, &subOrignalEndIndex);
+                if (-1 == subOrignalEndIndex)
+                    subOrignalEndIndex = resultName->GetLength();
+                Int32 subGoalEndIndex;
+                subsidyName->IndexOf(String(","), subsidyEqualIndex, &subGoalEndIndex);
+                if (-1 == subGoalEndIndex)
+                    subGoalEndIndex = subsidyName->GetLength();
+                resultName->Replace(equalIndex + 1, subOrignalEndIndex,
+                        subsidyName->Substring(subsidyEqualIndex + 1,
+                                subGoalEndIndex));
+                AutoPtr<ICharSequence> pReplaceName;
+                oidMap->Get(csq, (IInterface**)(ICharSequence**)&pReplaceName);
+                String s;
+                pReplaceName->ToString(&s);
+                resultName->Replace(commaIndex + 1, equalIndex, s);
+            }
+            fromIndex = commaIndex;
+            subsidyFromIndex = subsidyEqualIndex - 1;
+        }
+        resultName->ToString(pName);
+        return NOERROR;
+    } else {
+        // throw new IllegalArgumentException("invalid format specified: " + format);
+        return E_ILLEGAL_ARGUMENT_EXCEPTION;
+    }
 }
 
 ECode CX500Principal::Equals(
@@ -114,6 +159,9 @@ ECode CX500Principal::Equals(
     /* [out] */ Boolean *result)
 {
     VALIDATE_NOT_NULL(result)
+    *result = FALSE;
+    VALIDATE_NOT_NULL(o)
+
     if ((CX500Principal*)(IX500Principal*)o == this) {
         *result = TRUE;
         return NOERROR;
@@ -136,15 +184,23 @@ ECode CX500Principal::Equals(
 ECode CX500Principal::GetName(
     /* [out] */ String * pName)
 {
-    //Apache...Todo later
-    /* return dn.getName(RFC2253); */
-    return E_NOT_IMPLEMENTED;
+    VALIDATE_NOT_NULL(pName);
+    *pName = String(NULL);
+
+#if 0 // TODO: Waiting for Iname
+    return mDn->GetName(RFC2253, pName);
+#else
+    assert(0);
+    return NOERROR;
+#endif
 }
 
 ECode CX500Principal::GetHashCode(
     /* [out] */ Int32 *hashCode)
 {
     VALIDATE_NOT_NULL(hashCode)
+    *hashCode = 0;
+
     String name;
     GetCanonicalName(&name);
     *hashCode = name.GetHashCode();
@@ -154,47 +210,57 @@ ECode CX500Principal::GetHashCode(
 ECode CX500Principal::ToString(
     /* [out] */ String * pStr)
 {
-    //Apache...Todo later
-    /*
-        return dn.getName(RFC1779);
-    */
-    return E_NOT_IMPLEMENTED;
+    VALIDATE_NOT_NULL(pStr)
+    *pStr = String(NULL);
+
+#if 0 // TODO: Waiting for Iname
+    mDn->GetName(RFC1779, pStr);
+#else
+    assert(0);
+#endif
+    return NOERROR;
 }
 
 ECode CX500Principal::constructor(
     /* [in] */ ArrayOf<Byte> *name)
 {
+    VALIDATE_NOT_NULL(name)
+
     if (name == NULL) {
         return E_ILLEGAL_ARGUMENT_EXCEPTION;
     }
-    //Apache...Todo later
-    /*
-    try {
+    // try {
         // FIXME dn = new Name(name);
-        dn = (Name) Name.ASN1.decode(name);
-    } catch (IOException e) {
-        throw incorrectInputEncoding(e);
-    }
-    */
-    return E_NOT_IMPLEMENTED;
+#if 0 // TODO: Waiting for IName
+    return IName::ASN1->Probe(EIID_IASN1Type)->Decode(name, (IInterface**)&mDn);
+#else
+    assert(0);
+    return NOERROR;
+#endif
+    // } catch (IOException e) {
+        // throw incorrectInputEncoding(e);
+    // }
 }
 
 ECode CX500Principal::constructor(
     /* [in] */ IInputStream *in)
 {
+    VALIDATE_NOT_NULL(in)
+
     if (in == NULL) {
         return E_NULL_POINTER_EXCEPTION;
     }
-    //Apache...Todo later
-    /*
-    try {
+    // try {
         // FIXME dn = new Name(is);
-        dn = (Name) Name.ASN1.decode(in);
-    } catch (IOException e) {
-        throw incorrectInputEncoding(e);
-    }
-    */
-    return E_NOT_IMPLEMENTED;
+#if 0 // TODO: Waiting for IName
+    return IName::ASN1->Probe(EIID_IASN1Type)->Decode(in, (IInterface**)&mDn);
+#else
+    assert(0);
+    return NOERROR;
+#endif
+    // } catch (IOException e) {
+        // throw incorrectInputEncoding(e);
+    // }
 }
 
 ECode CX500Principal::constructor(
@@ -203,66 +269,57 @@ ECode CX500Principal::constructor(
     if (name.IsNull()) {
         return E_NULL_POINTER_EXCEPTION;
     }
-    //Apache...Todo later
-    /*
-    try {
-        dn = new Name(name);
-    } catch (IOException e) {
-        throw incorrectInputName(e, name);
-    }
-    */
-    return E_NOT_IMPLEMENTED;
+    // try {
+#if 0 // TODO: Waiting for IName
+    return IName::New(name, &mDn);
+#else
+    assert(0);
+    return NOERROR;
+#endif
+    // } catch (IOException e) {
+        // throw incorrectInputName(e, name);
+    // }
 }
 
 ECode CX500Principal::constructor(
     /* [in] */ const String& name,
     /* [in] */ IMap *keywordMap)
 {
+    VALIDATE_NOT_NULL(keywordMap)
+
     if (name.IsNull()) {
         return E_NULL_POINTER_EXCEPTION;
     }
-    //Apache...Todo later
-    /*
-    try {
-        dn = new Name(substituteNameFromMap(name, keywordMap));
-    } catch (IOException e) {
-        throw incorrectInputName(e, name);
-    }
-    */
-    return E_NOT_IMPLEMENTED;
+    // try {
+    String s;
+    FAIL_RETURN(SubstituteNameFromMap(name, keywordMap, &s));
+#if 0 // TODO: Waiting for Iname
+    return IName::New(s, &mDn);
+#else
+    assert(0);
+    return NOERROR;
+#endif
+    // } catch (IOException e) {
+        // throw incorrectInputName(e, name);
+    // }
 }
 
 ECode CX500Principal::GetCanonicalName(
         /* [out] */ String *name)
 {
     VALIDATE_NOT_NULL(name)
+    *name = String(NULL);
+
     AutoLock lock(mLock);
     if (mCanonicalName.IsNull()) {
-        //Apache...Todo later
-        //canonicalName = dn.getName(CANONICAL);
+#if 0 // TODO: Waiting for Iname
+        mDn->GetName(CANONICAL, &mCanonicalName);
+#else
+    assert(0);
+#endif
     }
     *name = mCanonicalName;
     return NOERROR;
-}
-
-ECode CX500Principal::WriteObject(
-    /* [in] */ IObjectOutputStream *out)
-{
-    //Apache...Todo later
-    /*
-        out.writeObject(dn.getEncoded());
-    */
-    return E_NOT_IMPLEMENTED;
-}
-
-ECode CX500Principal::ReadObject(
-    /* [in] */ IObjectInputStream *in)
-{
-    //Apache...Todo later
-    /*
-        dn = (Name) Name.ASN1.decode((byte[]) in.readObject());
-    */
-    return E_NOT_IMPLEMENTED;
 }
 
 ECode CX500Principal::SubstituteNameFromMap(
@@ -270,17 +327,21 @@ ECode CX500Principal::SubstituteNameFromMap(
     /* [in] */ IMap *keywordMap,
     /* [out] */ String *nm)
 {
-    StringBuilder sbName(name);
+    VALIDATE_NOT_NULL(nm)
+    *nm = String(NULL);
+    VALIDATE_NOT_NULL(keywordMap)
+
+    AutoPtr<StringBuilder> sbName = new StringBuilder(name);
     Int32 fromIndex;
-    sbName.GetLength(&fromIndex);
+    sbName->GetLength(&fromIndex);
     Int32 equalIndex;
     String tmpName;
     Int32 tmpIndex;
-    while (-1 != (equalIndex = (sbName.LastIndexOf(String("="), fromIndex, &tmpIndex), tmpIndex))) {
+    while (-1 != (equalIndex = (sbName->LastIndexOf(String("="), fromIndex, &tmpIndex), tmpIndex))) {
         Int32 commaIndex;
-        sbName.LastIndexOf(String(","), equalIndex, &commaIndex);
+        sbName->LastIndexOf(String(","), equalIndex, &commaIndex);
         String subName;
-        subName = (sbName.Substring(commaIndex + 1, equalIndex, &subName), subName).Trim();
+        subName = (sbName->Substring(commaIndex + 1, equalIndex, &subName), subName).Trim();
         AutoPtr<ICharSequence> key;
         CStringWrapper::New(subName, (ICharSequence**)&key);
         Boolean isContained;
@@ -290,15 +351,15 @@ ECode CX500Principal::SubstituteNameFromMap(
             keywordMap->Get(key, (IInterface**)&elem);
             String elemString;
             ICharSequence::Probe(elem)->ToString(&elemString);
-            sbName.Replace(commaIndex + 1, equalIndex, elemString);
+            sbName->Replace(commaIndex + 1, equalIndex, elemString);
         }
         fromIndex = commaIndex;
     }
-    return sbName.ToString(nm);
+    return sbName->ToString(nm);
 }
 
-}
-}
-}
-}
+} // namespace Callback
+} // namespace Auth
+} // namespace Security
+} // namespace Elastosx
 
