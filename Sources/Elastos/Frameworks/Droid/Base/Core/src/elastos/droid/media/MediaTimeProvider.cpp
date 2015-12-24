8dc9ff722b71d12deb588433cbd088e81a09fed5
BaoRongzhen <bao.rongzhen@kortide.com>
2015-12-24 09:48:32 +0800

    modify media

---
+++ Sources/Elastos/Frameworks/Droid/Base/Core/src/elastos/droid/media/MediaTimeProvider.cpp
@@ -0,0 +1,13 @@
+#include "elastos/droid/media/MediaTimeProvider.h"
+
+namespace Elastos {
+namespace Droid {
+namespace Media {
+
+CAR_INTERFACE_IMPL(MediaTimeProvider, Object, IMediaTimeProvider)
+CAR_INTERFACE_IMPL(MediaTimeProvider::OnMediaTimeListener, Object, IMediaTimeProviderOnMediaTimeListener)
+
+
+} // namespace Media
+} // namepsace Droid
+} // namespace Elastos
--- Sources/Elastos/Frameworks/Droid/Base/Core/src/elastos/droid/media/media.cpp
+++ Sources/Elastos/Frameworks/Droid/Base/Core/src/elastos/droid/media/media.cpp
@@ -1,5 +1,6 @@
 #include "elastos/droid/media/CAudioPort.h"
 #include "elastos/droid/media/CAudioPortConfig.h"
+#include "elastos/droid/media/CSubtitleController.h"
 
 namespace Elastos {
 namespace Droid {
@@ -8,6 +9,8 @@ namespace Media {
 CAR_OBJECT_IMPL(CAudioPort)
 
 CAR_OBJECT_IMPL(CAudioPortConfig)
+
+CAR_OBJECT_IMPL(CSubtitleController)
 
 } // namespace Media
 } // namepsace Droid
