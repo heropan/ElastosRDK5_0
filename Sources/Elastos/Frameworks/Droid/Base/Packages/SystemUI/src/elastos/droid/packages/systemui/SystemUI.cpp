5b36b6bc50aa920c12eb9c0d8ea378ad11d18e42
wanli <zhao.wanli@kortide.com>
2016-01-18 15:04:37 +0800

    1. Compile the app systemUI; 2. Delete DevSamples/SystemUI.

--- Sources/Elastos/Frameworks/Droid/Base/Packages/SystemUI/src/elastos/droid/packages/systemui/SystemUI.cpp
+++ Sources/Elastos/Frameworks/Droid/Base/Packages/SystemUI/src/elastos/droid/packages/systemui/SystemUI.cpp
@@ -1,9 +1,14 @@
-#include "elastos/droid/systemui/SystemUI.h"
+
+#include "elastos/droid/packages/systemui/SystemUI.h"
+#include "Elastos.Droid.Content.h"
+#include "Elastos.CoreLibrary.Utility.h"
 
 namespace Elastos {
 namespace Droid {
+namespace Packages {
 namespace SystemUI {
 
+CAR_INTERFACE_IMPL(SystemUI, Object, ISystemUI);
 SystemUI::SystemUI()
 {}
 
@@ -16,19 +21,73 @@ ECode SystemUI::OnConfigurationChanged(
     return NOERROR;
 }
 
+ECode SystemUI::Dump(
+    /* [in] */ IFileDescriptor* fd,
+    /* [in] */ IPrintWriter* pw,
+    /* [in] */ ArrayOf<String>* args)
+{
+    return NOERROR;
+}
+
+ECode SystemUI::OnBootCompleted()
+{
+    return NOERROR;
+}
+
+AutoPtr<IInterface> SystemUI::GetComponent(
+    /* [in] */ IClassInfo* interfaceType)
+{
+    if (mComponents == NULL) {
+        return NULL;
+    }
+    AutoPtr<IInterface> obj;
+    mComponents->Get(interfaceType, (IInterface**)&obj);
+    return obj;
+}
+
+ECode SystemUI::PutComponent(
+    /* [in] */ IClassInfo* interfaceType,
+    /* [in] */ IInterface* component)
+{
+    if (mComponents != NULL) {
+        mComponents->Put(interfaceType, component);
+    }
+    return NOERROR;
+}
+
 ECode SystemUI::SetContext(
     /* [in] */ IContext* context)
 {
-    assert(context != NULL && "Context can not be NULL.");
     mContext = context;
     return NOERROR;
 }
 
-AutoPtr<IContext> SystemUI::GetContext()
+ECode SystemUI::GetContext(
+    /* [out] */ IContext** context)
 {
-    return mContext;
+    VALIDATE_NOT_NULL(context);
+    *context = mContext;
+    REFCOUNT_ADD(*context);
+    return NOERROR;
 }
 
-}// namespace SystemUI
-}// namespace Droid
-}// namespace Elastos
+ECode SystemUI::SetComponents(
+    /* [in] */ IMap* components)
+{
+    mComponents = components;
+    return NOERROR;
+}
+
+ECode SystemUI::GetComponents(
+    /* [out] */ IMap** components)
+{
+    VALIDATE_NOT_NULL(components);
+    *components = mComponents;
+    REFCOUNT_ADD(*components);
+    return NOERROR;
+}
+
+} // namespace SystemUI
+} // namespace Packages
+} // namespace Droid
+} // namespace Elastos
