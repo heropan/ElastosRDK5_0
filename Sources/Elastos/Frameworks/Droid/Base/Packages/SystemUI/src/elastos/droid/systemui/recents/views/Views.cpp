aa449c3f586f519467e80d2027469e8bc24e934b
xihao <chen.xihao@kortide.com>
2016-02-18 09:33:13 +0000

    add systemui/recents/views

---
+++ Sources/Elastos/Frameworks/Droid/Base/Packages/SystemUI/src/elastos/droid/systemui/recents/views/Views.cpp
@@ -0,0 +1,22 @@
+
+#include "elastos/droid/systemui/recents/views/CDebugOverlayView.h"
+#include "elastos/droid/systemui/recents/views/CFixedSizeImageView.h"
+// #include "elastos/droid/systemui/recents/views/CRecentsView.h"
+
+namespace Elastos {
+namespace Droid {
+namespace SystemUI {
+namespace Recents {
+namespace Views {
+
+CAR_OBJECT_IMPL(CDebugOverlayView)
+
+CAR_OBJECT_IMPL(CFixedSizeImageView)
+
+// CAR_OBJECT_IMPL(CRecentsView)
+
+} // namespace Views
+} // namespace Recents
+} // namespace SystemUI
+} // namespace Droid
+} // namespace Elastos
